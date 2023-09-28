896cf668ef7ddb3df2608fbeb29ce47580f1ff2e
Matheus Morillo <matheus.morillo2000@gmail.com>
2023-09-28 15:32:12 -0300

    add test scenarios for lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -21,7 +21,7 @@ impl Config {
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let mut file = File::open(config.filename).expect("File could not be not found!");
+    let mut file = File::open(config.filename).expect("File could not be found!");
     let mut contents = String::new();
 
     file.read_to_string(&mut contents)?;
@@ -29,4 +29,69 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     println!("contents found: {}", contents);
 
     Ok(())
+}
+
+#[cfg(test)]
+mod config_tests {
+    use std::fs::File;
+
+    use super::run;
+
+    use super::Config;
+
+    #[test]
+    fn return_result_err_when_args_len_is_lower_than_three() {
+        let result = Config::new(&[String::from("arg1"), String::from("arg2")]);
+
+        assert!(result.is_err());
+        assert_eq!(
+            result.err().unwrap(),
+            "Expected 2 arguments (query, filename)"
+        )
+    }
+
+    #[test]
+    fn return_ok_along_with_config() {
+        let query = String::from("query");
+        let filename = String::from("filename");
+
+        let config = Config::new(&[String::from("exe"), query.clone(), filename.clone()]);
+
+        assert!(config.is_ok());
+
+        let config = config.unwrap();
+
+        assert_eq!(config.query, query);
+        assert_eq!(config.filename, filename);
+    }
+
+    #[test]
+    fn return_ok_given_config() {
+        let test_file = testfile::generate_name();
+        let _ = File::create(&test_file);
+
+        println!("Creating file with name: {}", test_file.to_str().unwrap());
+
+        let config = Config::new(&[
+            String::from("exe"),
+            String::from("query"),
+            String::from(test_file.to_str().unwrap()),
+        ]);
+
+        let result = run(config.unwrap());
+
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    #[should_panic(expected = "File could not be found!")]
+    fn panic_when_file_not_found() {
+        let config = Config::new(&[
+            String::from("exe"),
+            String::from("query"),
+            String::from("unexisting.txt"),
+        ]);
+
+        let _ = run(config.unwrap());
+    }
 }
